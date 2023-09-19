065a98c86694b14293f66ecc4b0e92c87dded4d2
Miguel Sobreira <msobreira@ua.pt>
2023-09-19 13:23:17 +0100

    new file

---
+++ aula1/ex3/primosV2.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+#shebang line
+
+#function descriptions...
+
+#import modules
+import colorama 
+from colorama import Fore, Back, Style
+
+#initialize colorama
+colorama.init(autoreset=True)
+
+
+maximum_number = 10
+def isPrime(value):
+    for i in range(2,value):
+        if value%i==0:
+            return False
+        
+    return True
+
+
+def main():
+
+    print("Starting to compute prime numbers up to " + str(maximum_number))
+    
+
+    for i in range(0, maximum_number):
+        if isPrime(i):
+            print('Number ' + Fore.GREEN + str(i) + Style.RESET_ALL +  ' is prime.')
+        else:
+            print('Number ' + str(i) + ' is not prime.')
+            print('Dividers used:'),
+            for k in range(0,i):
+                print(str(k), end=" ")
+            print("")
+
+if __name__ == "__main__":
+    main()
