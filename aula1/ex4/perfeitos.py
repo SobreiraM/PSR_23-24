53a8326652ddd524eec7e08340f98626bfdb11f6
Miguel Sobreira <msobreira@ua.pt>
2023-09-19 13:41:56 +0100

    new file ex4

---
+++ aula1/ex4/perfeitos.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+#shebang line
+
+
+maximum_number = 100
+
+def isPerfect(value):
+    sum = 0
+
+    for i in range(1,value):
+        if value%i==0:
+            sum += i   #somar numeros cuja divisão por value tenha resto 0
+            
+        if sum==value:
+            return True
+        
+    return False
+
+def main():
+    print("Starting to compute perfect numbers up to " + str(maximum_number))
+
+    for i in range(0, maximum_number):
+        if isPerfect(i):
+            print('Number ' + str(i) + ' is perfect.')
+
+
+if __name__ == "__main__":
+    main()
